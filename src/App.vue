a1a7c8b8c1293d3828f44b0d80f74dc957a2c39e
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-22 23:52:53 +0200

    add reset css and font

--- src/App.vue
+++ src/App.vue
@@ -38,11 +38,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
