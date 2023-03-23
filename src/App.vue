66f86dddf11cbdb644d0b0b9d8158c7d45733aa7
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-23 12:54:10 +0200

    finish 10

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
-  <div id="app">
-    <ApartmentsItem
-      :price="1000"
-      imgSrc="https://thumbs.dreamstime.com/b/hotel-13341433.jpg"
-      :descr="apartment.descr"
-      :rating="apartment.rating" />
-  </div>
+  <ContainerPage>
+    <div id="app">
+      <ApartmentsList :items="apartments" />
+    </div>
+  </ContainerPage>
 </template>
 <script>
-import ApartmentsItem from "./components/apartment/ApatmentsItem.vue";
+import ApartmentsList from "./components/apartment/ApartmentsList.vue";
+import ContainerPage from "./components/shared/ContainerPage.vue";
+import apartments from "./components/apartment/apartment";
 export default {
   name: "App",
-  components: { ApartmentsItem },
+  components: { ContainerPage, ApartmentsList },
   data() {
     return {
-      apartment: {
-        id: "1234",
-        title: "Item",
-        descr: "lorem",
-        price: 2032,
-        rating: 4.7,
-        location: {
-          city: "Zhytomyr",
-        },
-        owner: {
-          name: "Elen",
-          phone: "063-55-55-666",
-          email: "qwerty@gmail.com",
-        },
-      },
+      apartments,
     };
   },
 };
