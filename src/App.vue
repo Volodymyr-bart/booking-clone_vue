02a2a759dd75b01992f1c477b9ffd44c0bfba05c
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2023-03-24 13:49:40 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div class="app">
+  <div id="app">
     <h2>{{ text }}</h2>
     <ContainerPage>
-      <ApartmentsFilterForm class="apartments-filter" @submit="handleItemClick" />
+      <ApartmentsFilterForm class="apartments-filter" @submit="filter" />
     </ContainerPage>
-    <ApartmentsList :items="apartments">
+    <p v-if="!filteredApartments.length">Ничего не найдено</p>
+    <ApartmentsList v-else :items="filteredApartments">
       <template v-slot:apartment="{ apartment }">
         <ApartmentsItem
           :key="apartment.id"
           :descr="apartment.descr"
           :rating="apartment.rating"
-          :price="apartment.price"
-          :imgSrc="apartment.imgUrl" />
+          :imgSrc="apartment.imgUrl"
+          :price="apartment.price" />
       </template>
     </ApartmentsList>
   </div>
 </template>
+
 <script>
 import ApartmentsList from "./components/apartment/ApartmentsList.vue";
 import ApartmentsItem from "./components/apartment/ApatmentsItem.vue";
-import ApartmentsFilterForm from "./components/apartment/ApartmentsFilterForm.vue";
-import apartments from "./components/apartment/apartment";
-import ContainerPage from "./components/shared/ContainerPage.vue";
+import apartments from "./components/apartment/apartments";
+import ApartmentsFilterForm from "./components/apartment/ApartmentsFilterForm";
+import ContainerPage from "./components/shared/ContainerPage";
+
 export default {
   name: "App",
-  components: { ApartmentsList, ApartmentsItem, ContainerPage, ApartmentsFilterForm },
+  components: {
+    ApartmentsList,
+    ApartmentsItem,
+    ApartmentsFilterForm,
+    ContainerPage,
+  },
   data() {
     return {
       text: "",
@@ -35,25 +43,26 @@ export default {
       },
     };
   },
-
   computed: {
-    filtredApartments() {
+    filteredApartments() {
       return this.filterByCityName(this.filterByPrice(this.apartments));
     },
   },
   methods: {
-    handleItemClick({ city, price }) {
-      this.filters.city === city;
-      this.filters.price === price;
+    filter({ city, price }) {
+      this.filters.city = city;
+      this.filters.price = price;
     },
     filterByCityName(apartments) {
       if (!this.filters.city) return apartments;
+
       return apartments.filter((apartment) => {
         return apartment.location.city === this.filters.city;
       });
     },
     filterByPrice(apartments) {
       if (!this.filters.price) return apartments;
+
       return apartments.filter((apartment) => {
         return apartment.price >= this.filters.price;
       });
@@ -62,13 +71,17 @@ export default {
 };
 </script>
 
-<style>
-.app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+<style lang="scss" scoped>
+#app {
   font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.apartments-filter {
+  margin-bottom: 40px;
 }
 </style>
